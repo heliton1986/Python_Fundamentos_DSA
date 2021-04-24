13e8d9244bd0ad9cac8dfee5bd94b9b02ace84b2
Heliton Ribeiro Ferreira <heliton.ds86@gmail.com>
2021-04-24 19:12:39 -0300

    ATUALIZANDO

--- relatorio.py
+++ relatorio.py
@@ -1,4 +1,4 @@
 def relatorio(palavra):
     return len(palavra.split())
 
-print(relatorio("ola teste"))
+print(relatorio("Porque tenho muito interesse em fazer a trilha de cursos na área de Engenharia de Dados, porém estou desempregado e por isso não tenho condições de pagar os cursos que são de grande interesse para minha formação, só que no momento não disponho de recursos financeiros, por esse motivo peço o auxílio financeiro."))
